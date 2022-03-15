1fef85f3154d58e9a82b45a5e4fbacf542f82057
Ajay Singh <ajay10mar96@gmail.com>
2022-03-15 12:24:42 +0530

    adding remove function to linked list

--- app.js
+++ app.js
@@ -5,4 +5,7 @@ let l = new LinkedList();
 l.add(10);
 l.add(20);
 l.add(30);
-console.log(l.toString());
+
+l.print();
+console.log(l.removeByIndex(3));
+l.print();
--- linkedList/index.js
+++ linkedList/index.js
@@ -9,6 +9,10 @@ export class Node {
   toString() {
     return `<Node Data: ${this.data}>`;
   }
+
+  print() {
+    console.log(this.toString());
+  }
 }
 
 export default class LinkedList {
@@ -16,10 +20,21 @@ export default class LinkedList {
 
   constructor() {}
 
+  /**
+   * Returns true if there is no node in the LinkedList
+   * Otherwise return false
+   *
+   * @returns boolean
+   */
   isEmpty() {
-    return this.head === undefined || this.head === null;
+    return this.head == null;
   }
 
+  /**
+   * Returns the number of nodes in LinkedList
+   *
+   * @returns {number} count
+   */
   size() {
     let current = this.head,
       count = 0;
@@ -32,37 +47,148 @@ export default class LinkedList {
     return count;
   }
 
+  /**
+   * Adds new node with data at head of list
+   * Takes O(1) time
+   *
+   * @param {*} data
+   */
   add(data) {
-    /**
-     * Adds new node with data at head of list
-     * Takes O(1) time
-     */
     let newNode = new Node(data);
     newNode.nextNode = this.head;
     this.head = newNode;
   }
 
+  /**
+   * Search for the first node containing the data that matches the key
+   *
+   * @param {*} key
+   * @returns {Node | null}
+   */
+  search(key) {
+    let current = this.head;
+
+    while (current) {
+      if (current.data === key) return current;
+      else current = current.nextNode;
+    }
+
+    return null;
+  }
+
+  /**
+   * Inserts a new Node at index position
+   * Insertion Takes O(1) time
+   * Finding insertion position Takes O(n) time
+   * Overall Takes O(n) time
+   *
+   * @param {Node} data
+   * @param {number} index
+   */
+  insert(data, index = 0) {
+    if (index == 0) {
+      return this.add(data);
+    } else if (index > 0) {
+      let newNode = new Node(data),
+        position = index,
+        current = this.head;
+
+      while (position > 1) {
+        current = current.nextNode;
+        position -= 1;
+        if (current.nextNode == null) break;
+      }
+
+      let prevNode = current,
+        nextNode = current.nextNode;
+
+      prevNode.nextNode = newNode;
+      newNode.nextNode = nextNode;
+    }
+  }
+
+  /**
+   * Remove the Node containing data matching key
+   *
+   * @param {*} key
+   * @returns {Node | null} removed node
+   */
+  removeByKey(key) {
+    let current = this.head,
+      prevNode = null,
+      found = false;
+
+    while (current && !found) {
+      if (current.data === key && current === this.head) {
+        found = true;
+        this.head = current.nextNode;
+      } else if (current.data === key) {
+        found = true;
+        prevNode.nextNode = current.nextNode;
+      } else {
+        prevNode = current;
+        current = current.nextNode;
+      }
+    }
+
+    return found ? current : null;
+  }
+
+  /**
+   *
+   * @param {number} index
+   * @returns {Node | null} removed node
+   */
+  removeByIndex(index) {
+    let current = this.head;
+
+    if (index === 0) {
+      this.head = current.nextNode;
+      return current;
+    } else if (index > 0) {
+      let prevNode = current,
+        position = index;
+      while (position >= 1) {
+        prevNode = current;
+        current = current.nextNode;
+        position -= 1;
+      }
+      if (current != null) {
+        let nextNode = current.nextNode;
+        prevNode.nextNode = nextNode;
+      }
+    }
+
+    return current;
+  }
+
+  /**
+   * Converts the LinkedList into its string equivalent
+   *
+   * @returns {string}
+   */
   toString() {
     let current = this.head,
       nodes = [];
+
     while (current) {
       if (current === this.head) {
         nodes.push(`[Head: ${current.data}]`);
-      } else if (current.nextNode === undefined || current.nextNode === null) {
+      } else if (current.nextNode == null) {
         nodes.push(`[Tail: ${current.data}]`);
       } else {
         nodes.push(`[${current.data}]`);
       }
       current = current.nextNode;
     }
-    return nodes.toString();
+
+    return nodes.join("-> ");
   }
 
-  printAllNodeData() {
-    let current = this.head;
-    while (current) {
-      console.log(current.data);
-      current = current.nextNode;
-    }
+  /**
+   * Print the string equivalent of the LinkedList
+   */
+  print() {
+    console.log(this.toString());
   }
 }
